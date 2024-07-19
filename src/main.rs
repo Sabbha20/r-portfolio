9285aa880762f3e9a093c39fd27db45c3f984976
Sabbha20 <sabbha.power1@gmail.com>
2024-07-19 13:36:51 +0530

    making the portfolio as SPA

--- src/main.rs
+++ src/main.rs
@@ -16,38 +16,44 @@ async fn home(tmpl: web::Data<Tera>) -> impl Responder {
     context.insert("title", "Home Page");
     context.insert("message", "If you are looking for");
     context.insert("data", &p_data());
-    let rendered = tmpl.render("index.html", &context).unwrap();
+    let rendered = tmpl.render("spa-index.html", &context).unwrap();
+    // Commented for SPA
+    // let rendered = tmpl.render("index.html", &context).unwrap();
     HttpResponse::Ok().body(rendered)
 }
 
 
-async fn about(tmpl: web::Data<Tera>) -> impl Responder {
-    // HttpResponse::Ok().body("About Sabbha Mondal")
-    let mut context = Context::new();
-    context.insert("title", "About Page");
-    context.insert("data", &p_data());
-    let rendered = tmpl.render("about.html", &context).unwrap();
-    HttpResponse::Ok().body(rendered)
-}
+// Commented for SPA
+//
+// async fn about(tmpl: web::Data<Tera>) -> impl Responder {
+//     // HttpResponse::Ok().body("About Sabbha Mondal")
+//     let mut context = Context::new();
+//     context.insert("title", "About Page");
+//     context.insert("data", &p_data());
+//     let rendered = tmpl.render("about.html", &context).unwrap();
+//     HttpResponse::Ok().body(rendered)
+// }
+//
+// // async fn projects() -> impl Responder {
+// async fn projects(tmpl: web::Data<Tera>) -> impl Responder {
+//     // HttpResponse::Ok().body("Projects will be listed here")
+//     let mut context = Context::new();
+//     context.insert("title", "Contact Page");
+//     context.insert("data", &p_data());
+//     let rendered = tmpl.render("projects.html", &context).unwrap();
+//     HttpResponse::Ok().body(rendered)
+// }
+//
+// async fn contact(tmpl: web::Data<Tera>) -> impl Responder {
+//     // HttpResponse::Ok().body("@mondal_sabbha contact")
+//     let mut context = Context::new();
+//     context.insert("title", "Contact Page");
+//     context.insert("data", &p_data());
+//     let rendered = tmpl.render("contact.html", &context).unwrap();
+//     HttpResponse::Ok().body(rendered)
+// }
+// Commented for SPA
 
-// async fn projects() -> impl Responder {
-async fn projects(tmpl: web::Data<Tera>) -> impl Responder {
-    // HttpResponse::Ok().body("Projects will be listed here")
-    let mut context = Context::new();
-    context.insert("title", "Contact Page");
-    context.insert("data", &p_data());
-    let rendered = tmpl.render("projects.html", &context).unwrap();
-    HttpResponse::Ok().body(rendered)
-}
-
-async fn contact(tmpl: web::Data<Tera>) -> impl Responder {
-    // HttpResponse::Ok().body("@mondal_sabbha contact")
-    let mut context = Context::new();
-    context.insert("title", "Contact Page");
-    context.insert("data", &p_data());
-    let rendered = tmpl.render("contact.html", &context).unwrap();
-    HttpResponse::Ok().body(rendered)
-}
 
 async fn get_phrases() -> impl Responder {
     let phrases = &p_data().tag.clone();
@@ -68,9 +74,11 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(tera.clone())) // Make tera available to handlers
             .service(fs::Files::new("/static", "./static").show_files_listing()) // Serve static files
             .route("/", web::get().to(home))
-            .route("/about", web::get().to(about))
-            .route("/contact", web::get().to(contact))
-            .route("/projects", web::get().to(projects))
+            // Commented for SPA
+            // .route("/about", web::get().to(about))
+            // .route("/contact", web::get().to(contact))
+            // .route("/projects", web::get().to(projects))
+            // Commented for SPA
             .route("/phrases", web::get().to(get_phrases))
             .configure(admin::config)
     })
